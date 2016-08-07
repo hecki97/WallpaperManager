53a747eb2c67e8fa37631f1823eae4ae805c5d8b
Yannic Heckmann <heckinator97@gmx.de>
2016-08-07 22:28:33 +0200

    - Added drag and drop support for datagrid rows (disabled by default, can be activated by right clicking on the datagrid and choosing the option "Enable Drag and Drop (Experimental)" - The interval combobox now uses a valueconverter instead of a custom class - Various GUI tweaks for a (hopefully) better user experience - Renamed "Circle" to "Slideshow" - Added regions for (slightly) increased code readability
    Unfinished features:
    - Enable/Disable items by clicking on the checkbox only works in Picture mode

--- WallpaperManager/MainWindow.xaml.cs
+++ WallpaperManager/MainWindow.xaml.cs
@@ -8,12 +8,12 @@ using System.IO;
 using System.Windows.Controls;
 using System.Windows.Threading;
 using System.Collections.ObjectModel;
-using System.Drawing;
-using System.Xml;
-using System.Windows.Media;
+using System.Windows.Input;
+using System.Diagnostics;
 
 namespace WallpaperManager
 {
+    #region DataGridItem
     public class DataGridItem : IEquatable<DataGridItem>
     {
         public bool IsEnabled { get; set; }
@@ -48,29 +48,34 @@ namespace WallpaperManager
             return (Name == other.Name) && (Path == other.Path) && (Resolution == other.Resolution) && (Size == other.Size) && (Type == other.Type);
         }
     }
-
-    public class IntervalComboBoxItem
-    {
-        public string Key { get; set; }
-        public int Value { get; set; }
-
-        public IntervalComboBoxItem(string key, int value)
-        {
-            Key = key;
-            Value = value;
-        }
-    }
-
+    #endregion
+   
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        #region DraggedItem
+        /// <summary>
+        /// DraggedItem Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty DraggedItemProperty = DependencyProperty.Register("DraggedItem", typeof(DataGridItem), typeof(MainWindow));
+
+        /// <summary>
+        /// Gets or sets the DraggedItem property.
+        /// </summary>
+        public DataGridItem DraggedItem
+        {
+            get { return (DataGridItem)GetValue(DraggedItemProperty); }
+            set { SetValue(DraggedItemProperty, value); }
+        }
+        #endregion
+
         public new enum Background : int 
         {
             SolidColor,
             Picture,
-            Circle
+            Slideshow
         }
     
         private readonly List<string> supportedExtensions = new List<string> { ".JPG", ".JPEG", ".BMP", ".GIF", ".PNG" };
@@ -80,19 +85,20 @@ namespace WallpaperManager
         private Background background = Background.SolidColor;
 
         private DispatcherTimer dispatcherTimer = new DispatcherTimer(DispatcherPriority.SystemIdle);
-        private long counter = 0;
-        private int interval = 10;
+        private double counter = 0;
+        private double interval = 10;
 
         private ObservableCollection<DataGridItem> dataGridData = new ObservableCollection<DataGridItem>();
-        private ObservableCollection<IntervalComboBoxItem> intervalComboBoxData1 = new ObservableCollection<IntervalComboBoxItem>() { {new IntervalComboBoxItem("10 Sec", 10)}, { new IntervalComboBoxItem("15 Sec", 15)}, {new IntervalComboBoxItem("1 Min", 60)}, {new IntervalComboBoxItem("5 Mins", 300)} };
-
-        private ObservableCollection<Tuple<string, int>> intervalComboBoxData = new ObservableCollection<Tuple<string, int>>() { new Tuple<string, int>("15 Sec", 15), new Tuple<string, int>("30 Sec", 30), new Tuple<string, int>("1 Min", 60), new Tuple<string, int>("5 Min", 300), new Tuple<string, int>("15 Min", 900)};
+        private ObservableCollection<double> intervalComboBoxData = new ObservableCollection<double>() { 15, 30, 60, 300, 900, 3600, 86400, 604750, 604800 };
 
         private string applicationDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\WallpaperManager";
 
         private DataGridItem lastItem = new DataGridItem();
+        private WallpaperHandler.Style lastUsedStyle;
 
-        private int dirCount = 0; private int fileCount = 0; private int timeCount = 0;
+        private bool datagridHasItems = true; 
+
+        private int dirCount = 0; private int fileCount = 0; private Stopwatch stopwatch = new Stopwatch();
 
         public MainWindow()
         {
@@ -102,7 +108,7 @@ namespace WallpaperManager
             IntervalComboBox.DataContext = intervalComboBoxData;
 
             //Save and retrieve last selected Item
-            interval = intervalComboBoxData[0].Item2;
+            interval = intervalComboBoxData[0];
             wallpaperStyle = 0;
             background = 0;
 
@@ -159,6 +165,7 @@ namespace WallpaperManager
             UpdateDataGridGUI();
         }
 
+        #region Dispatcher Timer
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
@@ -181,6 +188,7 @@ namespace WallpaperManager
                 }
             }
         }
+        #endregion
 
         private void switchTheme_Click(object sender, RoutedEventArgs e)
         {
@@ -225,17 +233,26 @@ namespace WallpaperManager
         }
         */
 
+        private void ResetCount()
+        {
+            dirCount = 0; fileCount = 0; stopwatch.Reset();
+        }
+
         private void ButtonOpenDir_Click(object sender, RoutedEventArgs e)
         {
+            ResetCount();
             System.Windows.Forms.FolderBrowserDialog objDialog = new System.Windows.Forms.FolderBrowserDialog();
             objDialog.ShowDialog();
+            stopwatch.Start();
             if (!string.IsNullOrWhiteSpace(objDialog.SelectedPath)) {
                 TreeScan(objDialog.SelectedPath);
             } else {
                 DebugLog.Warning("No directory selected!");
             }
+            stopwatch.Stop();
             UpdateGUI();
             UpdateDataGridGUI();
+            label.Content = "Scanned " + dirCount + " directories and " + fileCount + " files in " + stopwatch.Elapsed.Seconds + " seconds";
         }
 
         private void ButtonAddFile_Click(object sender, RoutedEventArgs e) {
@@ -253,34 +270,13 @@ namespace WallpaperManager
             UpdateDataGridGUI();
         }
 
-        private void dataGrid_Drop(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
-                List<string> filepathList = new List<string>((string[])e.Data.GetData(DataFormats.FileDrop, true));
-                if (filepathList.Count > 0)
-                {
-                    List<string> tmp_list = new List<string>();
-                    foreach (string filepath in filepathList)
-                    {
-                        if (Directory.Exists(filepath))
-                            TreeScan(filepath);
-                        else if (File.Exists(filepath))
-                            AddFilesToList(new[] { filepath });
-                    }
-                    UpdateGUI();
-                    UpdateDataGridGUI();
-                }
-                else
-                    DebugLog.Log("There were no files in filedrop!");
-            }
-        }
-
         private void AddFilesToList(string[] files)
         {
             if (files == null || (!files.Any())) { DebugLog.Warning("No file(s) were selected!"); return; }
 
            foreach (string file in files) {
                 if (string.IsNullOrWhiteSpace(file)) continue;
+                fileCount++;
                 if (supportedExtensions.Contains(Path.GetExtension(file).ToUpperInvariant())) {
                     var img = System.Drawing.Image.FromFile(file);
                     DataGridItem item = new DataGridItem(true, Path.GetFileNameWithoutExtension(file), file, string.Format("{0}x{1}", img.Width, img.Height), Utilities.GetFileSizeOnDisk(file), Path.GetExtension(file).Substring(1).ToUpper());
@@ -310,6 +306,7 @@ namespace WallpaperManager
             foreach (string dir in Directory.GetDirectories(path))
             {
                 try {
+                    dirCount++;
                     TreeScan(dir); //Recursive call to get subdirs
                 }
                 catch (UnauthorizedAccessException e) {
@@ -319,13 +316,15 @@ namespace WallpaperManager
             }
         }
 
+        #region DataGrid Events
+
         private void dataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             if (background == Background.SolidColor) background = Background.Picture;
             DataGridItem item = (DataGridItem)dataGrid.SelectedItem;
             if (item != null && !string.IsNullOrWhiteSpace(item.Path))
             {
-                if (!lastItem.Path.Equals(item.Path) && item.IsEnabled)
+                if (!lastItem.Path.Equals(item.Path) || (lastItem.Path.Equals(item.Path) && !lastUsedStyle.Equals(wallpaperStyle)) && item.IsEnabled)
                 {
                     var wmcolor = ColorPicker.SelectedColor.Value;
                     WallpaperHandler.Set(item.Path, wallpaperStyle, System.Drawing.Color.FromArgb(wmcolor.A, wmcolor.R, wmcolor.G, wmcolor.B));
@@ -334,6 +333,7 @@ namespace WallpaperManager
                     //When double clicking on an item the timer will be reset to zero
                     counter = 0;
 
+                    lastUsedStyle = wallpaperStyle;
                     lastItem = item;
                     UpdateGUI();
                 }
@@ -343,6 +343,64 @@ namespace WallpaperManager
             else
                 DebugLog.Error("Item couldn't be selected because it is null or the path is missing!");
         }
+
+        private void dataGrid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                List<string> filepathList = new List<string>((string[])e.Data.GetData(DataFormats.FileDrop, true));
+                if (filepathList.Count > 0)
+                {
+                    List<string> tmp_list = new List<string>();
+                    foreach (string filepath in filepathList)
+                    {
+                        if (Directory.Exists(filepath))
+                            TreeScan(filepath);
+                        else if (File.Exists(filepath))
+                            AddFilesToList(new[] { filepath });
+                    }
+                    UpdateGUI();
+                    UpdateDataGridGUI();
+                }
+                else
+                    DebugLog.Log("There were no files in filedrop!");
+            }
+        }
+
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            int index = (DataGridHelper.TryFindParent<DataGridRow>((Button)sender)).GetIndex();
+
+            if (dataGridData.ElementAt(index) != null)
+                System.Diagnostics.Process.Start(dataGridData[index].Path);
+            else
+                DebugLog.Error("The Item which should be opened couldn't be found");
+        }
+
+        private void ButtonRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            int index = (DataGridHelper.TryFindParent<DataGridRow>((Button)sender)).GetIndex();
+
+            if (dataGridData.ElementAt(index) != null)
+            {
+                dataGridData.RemoveAt(index);
+                UpdateGUI();
+            }
+            else
+                DebugLog.Error("The Item which should be removed couldn't be found");
+        }
+
+        private void UpdateDataGridGUI()
+        {
+            if (dataGrid.HasItems == datagridHasItems) return;
+            datagridHasItems = dataGrid.HasItems;
+
+            for (int i = 0; i < dataGrid.Columns.Count; i++)
+            {
+                dataGrid.Columns[i].Visibility = (dataGrid.HasItems) ? Visibility.Visible : Visibility.Hidden;
+            }
+        }
+        #endregion
 
         private void ButtonChangeWP_Click(object sender, RoutedEventArgs e)
         {
@@ -362,7 +420,7 @@ namespace WallpaperManager
 
         private void IntervalComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            interval = ((Tuple<string,int>)IntervalComboBox.SelectedItem).Item2;
+            interval = (double)IntervalComboBox.SelectedItem;
             counter = 0;
         }
 
@@ -382,6 +440,8 @@ namespace WallpaperManager
             }
         }
 
+        int lastItemIndex = 0; bool useLastItemIndex = false;
+
         private void UpdateGUI()
         {
             UpdateButton(ButtonClearList);
@@ -394,10 +454,28 @@ namespace WallpaperManager
             BackgroundComboBox.SelectedIndex = (int)background;
 
             // Disable specific Items on specific datagriddata count
-            //((ComboBoxItem)BackgroundComboBox.Items[2]).IsEnabled = (dataGridData.Count > 1) ? true : false;
-            //((ComboBoxItem)BackgroundComboBox.Items[1]).IsEnabled = (dataGridData.Count > 0) ? true : false;
-            //TODO: Find better way to determine last selected item when datagriddata count <= 1 
-            //if (dataGridData.Count == 0) BackgroundComboBox.SelectedIndex = 0;
+            ((ComboBoxItem)BackgroundComboBox.Items[1]).IsEnabled = (dataGridData.Count > 0) ? true : false;
+            ((ComboBoxItem)BackgroundComboBox.Items[2]).IsEnabled = (dataGridData.Count > 1) ? true : false;
+
+            if (!useLastItemIndex && dataGridData.Count < 2)
+            {
+                if ((BackgroundComboBox.SelectedIndex == 1 && dataGridData.Count < 1) || (BackgroundComboBox.SelectedIndex == 2 && dataGridData.Count <= 1))
+                {
+                    useLastItemIndex = true;
+                    lastItemIndex = BackgroundComboBox.SelectedIndex;
+                }
+            }
+            else if (useLastItemIndex && dataGridData.Count >= 2)
+            {
+                if ((lastItemIndex == 1) || (lastItemIndex == 2))
+                {
+                    useLastItemIndex = false;
+                    BackgroundComboBox.SelectedIndex = lastItemIndex;
+                }
+            }
+
+            if (dataGridData.Count == 1 && useLastItemIndex) BackgroundComboBox.SelectedIndex = 1;
+            else if (dataGridData.Count < 1 && useLastItemIndex) BackgroundComboBox.SelectedIndex = 0;
 
             //Special Case
             if (background == Background.SolidColor && !ButtonChangeWP.IsEnabled) { ButtonChangeWP.IsEnabled = true; ButtonChangeWP.Opacity = 1; }
@@ -407,19 +485,6 @@ namespace WallpaperManager
         {
             button.IsEnabled = (dataGridData.Count > 0);
             button.Opacity = (button.IsEnabled) ? 1 : .5;   
-        }
-
-        private void ButtonRemoveItem_Click(object sender, RoutedEventArgs e)
-        {
-            int index = (DataGridHelper.TryFindParent<DataGridRow>((Button)sender)).GetIndex();
-
-            if (dataGridData.ElementAt(index) != null)
-            {
-                dataGridData.RemoveAt(index);
-                UpdateGUI();
-            }
-            else
-                DebugLog.Error("The Item which should be removed couldn't be found");
         }
 
         private bool GetEnumStringEnumType<TEnum>(string testString) where TEnum : struct
@@ -432,7 +497,7 @@ namespace WallpaperManager
         {
             background = (Background)BackgroundComboBox.SelectedIndex;
             if (dispatcherTimer.IsEnabled) { dispatcherTimer.Stop(); counter = 0; }
-            if (background == Background.Circle) dispatcherTimer.Start();
+            if (background == Background.Slideshow) dispatcherTimer.Start();
             if (background == Background.SolidColor) lastItem = new DataGridItem();
             UpdateGUI();
         }
@@ -444,27 +509,95 @@ namespace WallpaperManager
             dataGridData.Shuffle();
         }
 
-        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        #region DataGrid Drag and Drop Rows
+        private bool isDragging = false;
+       
+        private void dataGrid_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            int index = (DataGridHelper.TryFindParent<DataGridRow>((Button)sender)).GetIndex();
+            if (!isDragNDropEnabled) return; 
 
-            if (dataGridData.ElementAt(index) != null)
-                System.Diagnostics.Process.Start(dataGridData[index].Path);
-            else
-                DebugLog.Error("The Item which should be opened couldn't be found");
+            //find the clicked row
+            var row = DataGridHelper.TryFindFromPoint<DataGridRow>((UIElement)sender, e.GetPosition(dataGrid));
+            if (row == null) return;
+
+            //set flag that indicates we're capturing mouse movements
+            isDragging = true;
+            DraggedItem = (DataGridItem)row.Item;
         }
 
-        private bool datagridHasItems = true;
-
-        private void UpdateDataGridGUI()
+        /// <summary>
+        /// Completes a drag/drop operation.
+        /// </summary>
+        private void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            if (dataGrid.HasItems == datagridHasItems) return;
-            datagridHasItems = dataGrid.HasItems;
+            if (!isDragging) return;
 
-            for (int i = 0; i < dataGrid.Columns.Count; i++)
+            //get the target item
+            DataGridItem targetItem = (DataGridItem)dataGrid.SelectedItem;
+
+            if (targetItem == null || !ReferenceEquals(DraggedItem, targetItem))
             {
-                dataGrid.Columns[i].Visibility = (dataGrid.HasItems) ? Visibility.Visible : Visibility.Hidden;
+                //get target index
+                var targetIndex = dataGridData.IndexOf(targetItem);
+
+                //remove the source from the list
+                dataGridData.Remove(DraggedItem);
+
+                //move source at the target's location
+                dataGridData.Insert(targetIndex, DraggedItem);
+
+                //select the dropped item
+                dataGrid.SelectedItem = DraggedItem;
             }
+
+            //reset
+            ResetDragDrop();
         }
+
+        /// <summary>
+        /// Closes the popup and resets the
+        /// grid to read-enabled mode.
+        /// </summary>
+        private void ResetDragDrop()
+        {
+            isDragging = false;
+            popup.IsOpen = false;
+            dataGrid.IsReadOnly = false;
+        }
+
+        /// <summary>
+        /// Updates the popup's position in case of a drag/drop operation.
+        /// </summary>
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isDragging || e.LeftButton != MouseButtonState.Pressed) return;
+
+            //display the popup if it hasn't been opened yet
+            if (!popup.IsOpen)
+            {
+                //switch to read-only mode
+                dataGrid.IsReadOnly = true;
+
+                //make sure the popup is visible
+                popup.IsOpen = true;
+            }
+
+            Size popupSize = new Size(popup.ActualWidth, popup.ActualHeight);
+            popup.PlacementRectangle = new Rect(e.GetPosition(this), popupSize);
+
+            //make sure the row under the grid is being selected
+            Point position = e.GetPosition(dataGrid);
+            var row = DataGridHelper.TryFindFromPoint<DataGridRow>(dataGrid, position);
+            if (row != null) dataGrid.SelectedItem = row.Item;
+        }
+
+        private bool isDragNDropEnabled = false;
+
+        private void MenuItemDragNDrop_Click(object sender, RoutedEventArgs e)
+        {
+            isDragNDropEnabled = ((MenuItem)sender).IsChecked;
+            dataGrid.UnselectAllCells();
+        }
+        #endregion
     }
 }
--- WallpaperManager/ValueConverter.cs
+++ WallpaperManager/ValueConverter.cs
@@ -23,4 +23,27 @@ namespace WallpaperManager
             throw new NotImplementedException();
         }
     }
+
+    public class TimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TimeSpan span = TimeSpan.FromSeconds((double)value);
+
+            string formatted = string.Format("{0}{1}{2}{3}",
+               span.Duration().Days > 0 ? string.Format("{0:0} day{1}, ", span.Days, span.Days == 1 ? String.Empty : "s") : string.Empty,
+               span.Duration().Hours > 0 ? string.Format("{0:0} hour{1}, ", span.Hours, span.Hours == 1 ? String.Empty : "s") : string.Empty,
+               span.Duration().Minutes > 0 ? string.Format("{0:0} minute{1}, ", span.Minutes, span.Minutes == 1 ? String.Empty : "s") : string.Empty,
+               span.Duration().Seconds > 0 ? string.Format("{0:0} second{1}", span.Seconds, span.Seconds == 1 ? String.Empty : "s") : string.Empty);
+
+            if (formatted.EndsWith(", ")) formatted = formatted.Substring(0, formatted.Length - 2);
+            if (string.IsNullOrEmpty(formatted)) formatted = "0 seconds";
+            return formatted;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
